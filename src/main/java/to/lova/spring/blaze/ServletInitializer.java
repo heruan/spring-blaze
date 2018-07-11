7c7721bc4d8f82d1cac54eb73460b1db1fc298cf
Giovanni Lovato <giovanni.lovato@axians.it>
2018-07-11 22:58:58 +0200

    Initial commit

---
+++ src/main/java/to/lova/spring/blaze/ServletInitializer.java
@@ -0,0 +1,13 @@
+package to.lova.spring.blaze;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBlazeApplication.class);
+	}
+
+}
---
+++ src/main/java/to/lova/spring/blaze/SpringBlazeApplication.java
@@ -0,0 +1,12 @@
+package to.lova.spring.blaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlazeApplication.class, args);
+	}
+}
